2f73050274fb63e38ed919d3c9ca0ea62aa9d011
Hogyu, Kim <hgkimer@nara.co.kr>
2021-12-03 13:16:58 +0900

    Initial commit

---
+++ src/main/java/com/hgkimer/backboneexam/BackboneExamApplication.java
@@ -0,0 +1,13 @@
+package com.hgkimer.backboneexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackboneExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackboneExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgkimer/backboneexam/BackboneExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgkimer.backboneexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackboneExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
